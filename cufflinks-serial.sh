bc98d37fa7aae87b550575ab7d4c4018e5981f15
macklin <macklin@server>
2014-12-09 10:03:56 -0800

    Adding cufflinks-serial.sh.

---
+++ cufflinks-serial.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#########################
+## Import utility functions
+
+source util.sh
+
+#########################
+
+#########################
+## Check input
+
+# Make sure the user passes a valid TOPHAT_OUTPUT_DIR environmental variable
+if [ ! -d "${TOPHAT_OUTPUT_DIR}" ]; then
+	to_stderr "You must define the TOPHAT_OUTPUT_DIR environmental variable."
+	to_stderr "And TOPHAT_OUTPUT_DIR must exist as a directory."
+	exit 1
+fi
+
+# Make sure the user passes a valid CUFFLINKS_OUTPUT_DIR environmental variable
+if [ ! -d "${CUFFLINKS_OUTPUT_DIR}" ]; then
+	to_stderr "You must define the CUFFLINKS_OUTPUT_DIR environmental variable."
+	to_stderr "And CUFFLINKS_OUTPUT_DIR must exist as a directory."
+	exit 1
+fi
+
+#########################
+
+#########################
+## Process files
+
+echo "Processing BAM files in ${TOPHAT_OUTPUT_DIR}"
+echo
+
+echo "========================="
+
+find "${TOPHAT_OUTPUT_DIR}" -type f -name "*.bam" -print0 | \
+while IFS='' read -r -d '' FULL_FILE; do
+
+	# Get just the filename (strip off the directory)
+	BASE_FILE="$(basename $FULL_FILE)"
+
+	# Strip off the file extension
+	# See http://stackoverflow.com/a/965069/1647819
+	FILE_NO_EXT="${FULL_FILE%%.*}"
+
+	# Define output directory for this cufflinks run
+	THIS_OUTPUT_DIR="${CUFFLINKS_OUTPUT_DIR}/${FILE_NO_EXT}"
+
+	echo "+++++ Processing ${FULL_FILE}"
+	cufflinks -o "${THIS_OUTPUT_DIR}" "${FULL_FILE}"
+
+	echo
+done
+
+echo "========================="
+echo
+
+#########################
